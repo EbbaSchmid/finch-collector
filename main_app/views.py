2998b0584a228bc50de6b6f7ee9b52ae35e7efdf
EbbaSchmid <justebba@gmail.com>
2022-11-07 09:27:24 -0600

    part 1 css

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,21 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
+
+
+class Finch:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'yellow', 'Kinda rude.', 3),
+  Finch('Sachi', 'also yellow', 'Looks like a turtle.', 0),
+  Finch('Fancy', 'color', 'Happy fluff ball.', 4),
+  Finch('Bonk', 'color', 'Meows loudly.', 6)
+]
