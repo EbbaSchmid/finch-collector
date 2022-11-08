a3ec046f0f5bdb827d9c21413af2d758d39d1cd8
EbbaSchmid <justebba@gmail.com>
2022-11-08 12:39:40 -0600

    toy images

--- main_app/urls.py
+++ main_app/urls.py
@@ -36,6 +36,5 @@ urlpatterns = [
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
 
-  # associate a toy with a cat (M:M)
   path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
   ]
