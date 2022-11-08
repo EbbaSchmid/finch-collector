cafee16615001395511170c34bd36bd87e1043b7
EbbaSchmid <justebba@gmail.com>
2022-11-08 15:16:12 -0600

    signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,31 +1,33 @@
 from django.urls import path
 from . import views
 
+# localhost:8000/
+
+### NOT DOING THIS: localhost:8000/finches
+
 urlpatterns = [
   # localhost:8000/
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   # localhost:8000/about
-  path('about/', views.about ,name='about'),
+  path('about/', views.about, name='about'),
   # localhost:8000/finches
   path('finches/', views.finches_index, name='finches_index'),
-  # localhost:8000/finch/int:finch_id (to view the details of a single finch)
-  path('finches/<int:finch_id>/',views.finches_detail, name='finches_detail'),
+  # localhost:8000/finches/int:finch_id
+  path('finches/<int:finch_id>/', views.finches_detail, name='finches_detail'),
   # localhost:8000/finches/create
   path('finches/create/', views.FinchCreate.as_view(), name='finches_create'),
   # localhost:8000/finches/int:finch_id/update
   path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
-  # localhost:8000/finchess/int:finch_id/delete
-  path('finchess/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
-  
-  # localhost:8000/finchess/int:finch_id/add_feeding
+  # localhost:8000/finches/int:finch_id/delete
+  path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
+  # localhost:8000/finches/int:finch_id/add_feeding
   path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
-
   # localhost:8000/toys/create
   path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
-  path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+  path('toys/<int:pk>', views.ToyDetail.as_view(), name='toys_detail'),
   path('toys/', views.ToyList.as_view(), name='toys_index'),
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
-
-  path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
-  ]
+  path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name="assoc_toy"),
+  path('accounts/signup', views.signup, name='signup')
+]
